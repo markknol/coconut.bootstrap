63fc716cf881f325d7cdff516821d7ebfbd75ae9
Mark <mark.knol@gmail.com>
2020-04-30 10:39:38 +0200

    improve Alert - remove onclick / add onDismiss callback

--- src/bootstrap/Alert.hx
+++ src/bootstrap/Alert.hx
@@ -5,14 +5,14 @@ import coconut.vdom.View;
 class Alert extends View {
 	static inline final prefix: String = 'alert';
 
-	@:state var show: Bool = true;
+	@:state var show:Bool = true;
 
 	@:attribute var variant: Variant = Primary;
 	@:attribute var dismissible: Bool = false;
 	@:attribute var closeLabel: String = 'Close alert';
 	@:attribute var children:Children;
 
-	@:attribute function onclick():Void show = false; 
+	@:attribute function onDismiss():Void {};
 
 	function render() '
 		<if ${show}>
@@ -24,12 +24,17 @@ class Alert extends View {
 				]}
 			>
 				<if ${dismissible}>
-					<CloseButton onclick=${onclick} label=${closeLabel} />
+					<CloseButton onclick=${handleCloseButtonClick} label=${closeLabel} />
 				</if>
 				${...children}
 			</div>
 		</if>
 	';
+
+	private function handleCloseButtonClick():Void {
+		show = false;
+		onDismiss();
+	}
 
 	public static function Heading(attr:{children:Children}) {
 		return Bootstrap.create("alert-heading h4", attr.children);
